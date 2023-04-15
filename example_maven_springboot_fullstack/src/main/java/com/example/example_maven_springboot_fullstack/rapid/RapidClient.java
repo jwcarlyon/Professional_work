78c6530f71b9264cf08d7bfe842d45a26b93b8e2
jwcarlyon <jwcarlyon@yahoo.com>
2023-04-15 18:55:58 -0500

    + minor cleanup to RapidClient and test

--- example_maven_springboot_fullstack/src/main/java/com/example/example_maven_springboot_fullstack/rapid/RapidClient.java
+++ example_maven_springboot_fullstack/src/main/java/com/example/example_maven_springboot_fullstack/rapid/RapidClient.java
@@ -1,6 +1,7 @@
 package com.example.example_maven_springboot_fullstack.rapid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,8 @@ import java.util.Collections;
 
 @Service
 public class RapidClient {
+    @Value("${example-maven-springboot-fullstack.rapid-api.key}")
+    private String rapidApiKey;
     @Autowired
     private RestTemplate rapidAPI;
 
@@ -19,7 +22,7 @@ public class RapidClient {
     public String getDriverByName(String name) {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.add("X-RapidAPI-Key", "652de7616fmshb8c83991a39ea17p10cf3ajsn4abc51b02d5d");
+        headers.add("X-RapidAPI-Key", rapidApiKey);
         headers.add("X-RapidAPI-Host", "api-formula-1.p.rapidapi.com");
         HttpEntity<String> httpEntity = new HttpEntity<>(headers);
         ResponseEntity<String> response = rapidAPI.exchange(
--- example_maven_springboot_fullstack/src/test/java/com/example/example_maven_springboot_fullstack/rapid/RapidClientTest.java
+++ example_maven_springboot_fullstack/src/test/java/com/example/example_maven_springboot_fullstack/rapid/RapidClientTest.java
@@ -1,14 +1,13 @@
 package com.example.example_maven_springboot_fullstack.rapid;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
@@ -18,12 +17,16 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RapidClientTest {
     @Mock
     private RestTemplate mockRapidApi;
     @InjectMocks
     private RapidClient rapidClient;
-
+    @BeforeAll
+    void setup() {
+        ReflectionTestUtils.setField(rapidClient, "rapidApiKey", "testKey");
+    }
     @Nested
     class getDriverByName {
         @Test
@@ -33,7 +36,7 @@ class RapidClientTest {
             String expectedResponse = "{\"get\":\"drivers\",\"parameters\":{\"search\":\"russel\"},\"errors\":[],\"results\":1,\"response\":[{\"id\":51,\"name\":\"George Russell\",\"abbr\":\"RUS\",\"image\":\"https:\\/\\/media-3.api-sports.io\\/formula-1\\/drivers\\/51.png\",\"nationality\":\"British\",\"country\":{\"name\":\"United Kingdom\",\"code\":\"GB\"},\"birthdate\":\"1998-02-15\",\"birthplace\":\"King's Lynn, England\",\"number\":63,\"grands_prix_entered\":85,\"world_championships\":0,\"podiums\":9,\"highest_race_finish\":{\"position\":1,\"number\":1},\"highest_grid_position\":1,\"career_points\":\"312\",\"teams\":[{\"season\":2023,\"team\":{\"id\":5,\"name\":\"Mercedes-AMG Petronas\",\"logo\":\"https:\\/\\/media-2.api-sports.io\\/formula-1\\/teams\\/5.png\"}},{\"season\":2022,\"team\":{\"id\":5,\"name\":\"Mercedes-AMG Petronas\",\"logo\":\"https:\\/\\/media-2.api-sports.io\\/formula-1\\/teams\\/5.png\"}},{\"season\":2021,\"team\":{\"id\":12,\"name\":\"Williams F1 Team\",\"logo\":\"https:\\/\\/media-3.api-sports.io\\/formula-1\\/teams\\/12.png\"}},{\"season\":2020,\"team\":{\"id\":12,\"name\":\"Williams F1 Team\",\"logo\":\"https:\\/\\/media-3.api-sports.io\\/formula-1\\/teams\\/12.png\"}},{\"season\":2019,\"team\":{\"id\":12,\"name\":\"Williams F1 Team\",\"logo\":\"https:\\/\\/media-3.api-sports.io\\/formula-1\\/teams\\/12.png\"}}]}]}";
             HttpHeaders headers = new HttpHeaders();
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            headers.add("X-RapidAPI-Key", "652de7616fmshb8c83991a39ea17p10cf3ajsn4abc51b02d5d");
+            headers.add("X-RapidAPI-Key", "testKey");
             headers.add("X-RapidAPI-Host", "api-formula-1.p.rapidapi.com");
             HttpEntity<String> httpEntity = new HttpEntity<>(headers);
             when(mockRapidApi.exchange(
